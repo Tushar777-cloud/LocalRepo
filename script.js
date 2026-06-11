2db6448e28c7af99f6be61148f865a063cdafaae
Tushar777-cloud <tusharlaha777@gmail.com>
2026-06-11 20:33:56 +0545

    Added game and button

---
+++ script.js
@@ -0,0 +1,8 @@
+var a =0;
+function playGame(){
+    
+    a++;
+    if(a==1){
+        document.getElementById("Instructions").innerHTML = "Click the button again to see the next instruction.";
+    }
+}
